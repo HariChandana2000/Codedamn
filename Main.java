cfd8815145852fe7c8f78d0b1c16287f74b9c60e
Hari Chandana <gottuparthiharichandana@gmail.com>
2023-01-12 19:06:43 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,17 +2,6 @@ package MyStack;
 
 public class Main {
     public static void main(String[] args) {
-//        MyStack stack = new MyStack(5);
-//
-//        stack.push(1);
-//        stack.push(2);
-//        stack.push(3);
-//        stack.push(4);
-//        stack.push(5);
-//        stack.push(6);
-//
-//        System.out.println(stack);
-
         MyStackLinkedList stack = new MyStackLinkedList();
         stack.push(1);
         stack.push(2);
@@ -21,5 +10,6 @@ public class Main {
         System.out.println(stack);
         System.out.println("Peek: " + stack.peek());
         System.out.println("Size: " + stack.size());
+        System.out.println("Popped: " + stack.pop());
     }
 }
